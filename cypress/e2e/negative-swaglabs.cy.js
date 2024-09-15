4ceed5a3f1df999a20bf87440d96c1ab1ee13a6a
Muhammad Akbar <muhammad.akbar900201@bni.co.id>
2024-09-15 15:06:15 +0700

    feat: add swaglabs TC

---
+++ cypress/e2e/negative-swaglabs.cy.js
@@ -0,0 +1,29 @@
+describe("Negative - Swaglabs Web Page test", () => {
+  beforeEach(() => {
+    cy.visit("https://www.saucedemo.com/v1/index.html");
+    cy.url().should("include", "saucedemo");
+    cy.get('[data-test="username"]')
+      .should("be.visible")
+      .and("not.be.disabled");
+  });
+
+  it("User login with locked out user", () => {
+    cy.get('[data-test="username"]').type("locked_out_user");
+    cy.get('[data-test="password"]').type("secret_sauce");
+    cy.contains("LOGIN").should("be.visible").and("not.be.disabled").click();
+    cy.get('[data-test="error"]')
+      .should("exist")
+      .should("contain", "locked out");
+  });
+
+  it("User login with problem user", () => {
+    cy.get('[data-test="username"]').type("problem_user");
+    cy.get('[data-test="password"]').type("secret_sauce");
+    cy.contains("LOGIN").should("be.visible").and("not.be.disabled").click();
+    cy.url().should("include", "inventory");
+    cy.get(".app_logo").should("be.visible");
+    cy.get(
+      'img[src="./img/sauce-backpack-1200x1500.jpgWithGarbageOnItToBreakTheUrl"]'
+    ).should("exist");
+  });
+});
---
+++ cypress/e2e/swaglabs.cy.js
@@ -0,0 +1,98 @@
+describe("Swaglabs Web Page test", () => {
+  beforeEach(() => {
+    cy.visit("https://www.saucedemo.com/v1/index.html");
+    cy.url().should("include", "saucedemo");
+    cy.get('[data-test="username"]')
+      .should("be.visible")
+      .and("not.be.disabled")
+      .type("standard_user");
+    cy.get('[data-test="password"]').type("secret_sauce");
+    cy.contains("LOGIN").should("be.visible").and("not.be.disabled").click();
+    cy.url().should("include", "inventory");
+    cy.get(".app_logo").should("be.visible");
+  });
+
+  const addItemToCart = (nthChild) => {
+    cy.get(".inventory_list")
+      .children(`:nth-child(${nthChild})`)
+      .should("be.visible")
+      .then(($item) => {
+        cy.wrap($item)
+          .find(".pricebar > .btn_primary")
+          .click()
+          .should("contain", "REMOVE");
+      });
+  };
+
+  const removeItemFromCart = (nthChild) => {
+    cy.get(`:nth-child(${nthChild})`)
+      .children(".cart_item_label")
+      .should("be.visible")
+      .then(($item) => {
+        cy.wrap($item).find(".item_pricebar > .btn_secondary").click();
+      });
+  };
+
+  it("User success login", () => {});
+
+  it("User success add Backpack item to the cart", () => {
+    addItemToCart(1);
+  });
+
+  it("User success add Bike Light item to the cart", () => {
+    addItemToCart(2);
+  });
+
+  it("User success add Bolt T-Shirt item to the cart", () => {
+    addItemToCart(3);
+  });
+
+  it("User success add Fleece Jacket item to the cart", () => {
+    addItemToCart(4);
+  });
+
+  it("User success add Onesie item to the cart", () => {
+    addItemToCart(5);
+  });
+
+  it("User success add T-Shirt (Red) item to the cart", () => {
+    addItemToCart(6);
+  });
+
+  it("User success add All items to the cart", () => {
+    let i = 1;
+    let totalItem = 6;
+    for (i; i <= totalItem; i++) {
+      addItemToCart(i);
+    }
+    cy.get('[data-icon="shopping-cart"]').click();
+    cy.url().should("include", "cart");
+    cy.get(".shopping_cart_badge")
+      .invoke("text")
+      .then((cartItemCount) => {
+        cy.log(cartItemCount);
+        expect(cartItemCount).to.eq(`${totalItem}`);
+      });
+  });
+
+  it("User success remove All items to the cart", () => {
+    let i = 1;
+    let totalItem = 6;
+    for (i; i <= totalItem; i++) {
+      addItemToCart(i);
+    }
+    cy.get('[data-icon="shopping-cart"]').click();
+    cy.url().should("include", "cart");
+    cy.get(".shopping_cart_badge")
+      .invoke("text")
+      .then((cartItemCount) => {
+        cy.log(cartItemCount);
+        expect(cartItemCount).to.eq("6");
+      });
+
+    for (let i = 3; i < totalItem + 3; i++) {
+      removeItemFromCart(i);
+    }
+    cy.get(".cart_item_label").should("not.exist");
+  });
+});
